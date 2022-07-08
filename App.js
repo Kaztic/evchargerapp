15a2b3b85ff7398640da2cd7c3e9a6a5738f0084
kaztic <karthikbhat1225@gmail.com>
2022-07-08 16:04:16 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
+const latitude = 28.6304
+const longitude = 77.2177
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} 
+        initialRegion={{
+          latitude: 28.6304,
+          longitude: 77.2177,
+          latitudeDelta: 0.0222,
+          longitudeDelta: 0.0121,
+        
+        }}
+      />
+      <Marker
+        coordinate={{ latitude : latitude , longitude : longitude }}
+        image={{uri: 'https://www.vhv.rs/viewpic/bhwmJ_map-pin-pink-png-transparent-png/#'}}
+      />
     </View>
   );
 }
@@ -16,5 +30,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
